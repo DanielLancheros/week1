2840062db36443a3f269ee8e06deb4fdf4d9e376
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-06-09 16:59:40 -0400

    FIRST commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function concatenateName (parameterOne,parameterTwo) {
+    const result = parameterOne + parameterTwo;
+
+    return result;
+}
+
+const fullName = concatenateName ("firstName","lastName");
+
+
+
+//GET Help, not correct
